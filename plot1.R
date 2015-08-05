e136a1ab83fccf8e0d4921c72a44c9c165a41d91
cchang <chang.charles@epa.gov>
2015-08-05 16:00:02 -0400

    Working 4

--- plot1.R
+++ plot1.R
@@ -1,7 +1,27 @@
 
 # The data file is called household_power_consumption.txt
 # Set the fileName points to household_power_consumption.txt
+zipfileName <- "exdata_data_household_power_consumption.zip"
 fileName <- "household_power_consumption.txt"
+# (getwd())
+fileexist <- file.exists(fileName)
+(fileexist)
+
+if (! fileexist) {
+    # check to see if zipped data file is available
+    if (! file.exists(zipfileName)) {
+        # Retrieve the file from web
+        print("Retrieving file from web")
+        fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+        download.file(fileUrl,zipfileName)
+        # unzip file 
+        unzip(zipfileName)
+        if (file.exists(fileName))
+            print("data file available")
+    }
+} else
+    print("data file available")
+
 # Use read.table to read in the data set 
 # Read in header too and separator is ";"
 # Read in everything as character
@@ -16,27 +36,19 @@ dim(dt)
 # Find out the data structure for the variables
 str(dt)
 
-# Convert column
-# Converting the missing value ? to NA
-# dt$Global_active_power[dt$Global_active_power == "?"] <- NA
-# Retrieve 2007-02-01 data
-feb1 <- dt[dt$Date == "1/2/2007",]
-# Retrieve 2007-02-02 data
-feb2 <- dt[dt$Date == "2/2/2007",]
-
-# Combine feb1 and feb2 to become the base dataset 
-dt2 <- rbind(feb1,feb2)
+# Retrieve 2007-02-01 and 2007-02-02 data
+dt2 <- dt[dt$Date == "1/2/2007" | dt$Date == "2/2/2007",]
 
 # Clean up dataset
 rm(dt)
 
 # Create a new column
-dt2$newdate <- paste(dt2$Date,dt2$Time)
+dt2$datetime <- paste(dt2$Date,dt2$Time)
 
-# Convert the newdate to POSIX
+# Convert the datetime to POSIX
 # The reason to convert newdate from character to POSIX is,
 # POSIX actually is a integer number
-dt2$newdate <- strptime(dt2$newdate,"%d/%m/%Y %H:%M:%S")
+dt2$datetime <- strptime(dt2$datetime,"%d/%m/%Y %H:%M:%S")
 
 # Convert Global_active_power to numeric
 dt2$Global_active_power <- as.numeric(dt2$Global_active_power)
@@ -45,7 +57,8 @@ summary(dt2$Global_active_power)
 
 png(filename = "plot1.png",width = 480, height = 480)
 
-hist(dt2$Global_active_power,xlab="Global Active Power (kilowatts)",
-     main="Global Active Power",col="red")
+hist(dt2$Global_active_power,ann=FALSE,col="red")
+title(xlab="Global Active Power (kilowatts)",
+      ylab="Frequency",
+      main="Global Active Power")
 dev.off()
-
--- plot3.R
+++ plot3.R
@@ -1,8 +1,27 @@
-
 # The data file is called household_power_consumption.txt
 # Set the fileName points to household_power_consumption.txt
+zipfileName <- "exdata_data_household_power_consumption.zip"
 fileName <- "household_power_consumption.txt"
-# Use read.table to read in the data set 
+# (getwd())
+fileexist <- file.exists(fileName)
+(fileexist)
+
+if (! fileexist) {
+    # check to see if zipped data file is available
+    if (! file.exists(zipfileName)) {
+        # Retrieve the file from web
+        print("Retrieving file from web")
+        fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+        download.file(fileUrl,zipfileName)
+        # unzip file 
+        unzip(zipfileName)
+        if (file.exists(fileName))
+            print("data file available")
+    }
+} else
+    print("data file available")
+
+    # Use read.table to read in the data set 
 # Read in header too and separator is ";"
 # Read in everything as character
 dt <- read.table(fileName,header = T,sep=";",colClasses = "character")
@@ -18,45 +37,42 @@ str(dt)
 
 # Convert column
 # Converting the missing value ? to NA
-# dt$Global_active_power[dt$Global_active_power == "?"] <- NA
-# Retrieve 2007-02-01 data
-feb1 <- dt[dt$Date == "1/2/2007",]
-# Retrieve 2007-02-02 data
-feb2 <- dt[dt$Date == "2/2/2007",]
-
-# Combine feb1 and feb2 to become the base dataset 
-dt2 <- rbind(feb1,feb2)
-
+# Retrieve 2007-02-01 and 2007-02-02 data 
+dt2 <- dt[dt$Date == "1/2/2007" | dt$Date == "2/2/2007",]
 # Clean up dataset
 rm(dt)
 
 # Create a new column
-dt2$newdate <- paste(dt2$Date,dt2$Time)
+dt2$datetime <- paste(dt2$Date,dt2$Time)
 
-# Convert the newdate to POSIX
-# The reason to convert newdate from character to POSIX is,
+# Convert the datetime to POSIX
+# The reason to convert datetime from character to POSIX is,
 # POSIX actually is a integer number
-dt2$newdate <- strptime(dt2$newdate,"%d/%m/%Y %H:%M:%S")
+dt2$datetime <- strptime(dt2$datetime,"%d/%m/%Y %H:%M:%S")
 
 # Convert Sub_metering_1, Sub_metering_2 and Sub_metering_3 to numeric
 dt2$Sub_metering_1 <- as.numeric(dt2$Sub_metering_1)
 dt2$Sub_metering_2 <- as.numeric(dt2$Sub_metering_2)
 dt2$Sub_metering_3 <- as.numeric(dt2$Sub_metering_3)
 
-# Get summary of Sub_metering
-# summary(dt2$Sub_metering_1)
-# summary(dt2$Sub_metering_2)
-# summary(dt2$Sub_metering_3)
-
 png(filename = "plot3.png",width = 480, height = 480)
 
-# change the margin of the plot to hide xlab 
-# bottom lines is the first one in c(bottom,left,top,right)
-with(dt2,plot(newdate,Sub_metering_1,type="n",ann=FALSE))
+# Disable the label from axis ann=F
+# Set up plot but don't draw type="n"
+#   Need consider to find out the largest range for Y-axis
+r1 <- range(dt2$Sub_metering_1)
+r2 <- range(dt2$Sub_metering_2)
+r3 <- range(dt2$Sub_metering_3)
+v <- c(r1[2],r2[2],r3[2])
+ind <- which.max(v)
+subsetstr <- paste("Sub_metering_",ind)
+
+#with(dt2,plot(datetime,Sub_metering_1,type="n",ann=FALSE))
+
 title(ylab="Energy sub metering")
-with(dt2,lines(newdate,Sub_metering_1,type="l"))
-with(dt2,lines(newdate,Sub_metering_2,type="l",col="red"))
-with(dt2,lines(newdate,Sub_metering_3,type="l",col="blue"))
+with(dt2,lines(datetime,Sub_metering_1,type="l"))
+with(dt2,lines(datetime,Sub_metering_2,type="l",col="red"))
+with(dt2,lines(datetime,Sub_metering_3,type="l",col="blue"))
 legend("topright",col=c("black","red","blue"),
        legend=c("Sub_meterting_1","Sub_metering_2","Sub_metering_3"),
        lty=c(1.5,1.5,1.5))
